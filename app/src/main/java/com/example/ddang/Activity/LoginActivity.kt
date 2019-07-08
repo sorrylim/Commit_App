a382361eeb687ac7aeb5debbc460560fc06fa1e3
sorrylim <52535550+sorrylim@users.noreply.github.com>
2019-07-08 20:17:01 +0900

    폴더추가

--- app/src/main/java/com/example/ddang/Activity/LoginActivity.kt
+++ app/src/main/java/com/example/ddang/Activity/LoginActivity.kt
@@ -1,12 +1,13 @@
-package com.example.ddang
+package com.example.ddang.Activity
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.ddang.R
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 }
---
+++ app/src/main/java/com/example/ddang/Activity/SplashActivity.kt
@@ -0,0 +1,13 @@
+package com.example.ddang.Activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.ddang.R
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
