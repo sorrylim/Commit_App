f7334631cf4bf5339097017abc972b28196cffe9
sorrylim <52535550+sorrylim@users.noreply.github.com>
2019-08-07 18:18:39 +0900

    naver test

--- app/src/main/java/com/commit/ddang/Activity/MainActivity.kt
+++ app/src/main/java/com/commit/ddang/Activity/MainActivity.kt
@@ -1,66 +1,28 @@
 package com.commit.ddang.Activity
 
-import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
-import android.view.ViewGroup
-import androidx.appcompat.widget.SearchView
-import androidx.core.view.ViewCompat
-import com.commit.ddang.Fragment.AccountFragment
 import com.commit.ddang.Fragment.FavoriteFragment
 import com.commit.ddang.Fragment.HomeFragment
 import com.commit.ddang.Fragment.MapFragment
 import com.commit.ddang.R
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.google.android.material.snackbar.BaseTransientBottomBar
-import com.google.android.material.snackbar.Snackbar
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
-    var time: Long = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.main_bottom_navigation_view)
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.main_bnv)
         bottomNavigationView.setOnNavigationItemSelectedListener(navListener)
+
         if (savedInstanceState == null) {
             val fragment = HomeFragment()
             supportFragmentManager.beginTransaction()
-                .replace(R.id.main_fragment_view, fragment, fragment.javaClass.simpleName).commit()
+                .replace(R.id.main_frame, fragment, fragment.javaClass.simpleName).commit()
         }
-    }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        val inflater = menuInflater
-        inflater.inflate(R.menu.menu_home, menu)
-        val searchView = menu!!.findItem(R.id.action_search).actionView as SearchView
-        searchView.queryHint = "검색어를 입력하세요."
-        return true
-    }
-
-    override fun onBackPressed() {
-        val snack = Snackbar.make(main_layout, "'뒤로'버튼을 한 번 더 누르면 종료됩니다.", Snackbar.LENGTH_SHORT)
-            .setAnchorView(main_bottom_navigation_view).setAnimationMode(BaseTransientBottomBar.ANIMATION_MODE_FADE)
-        if (System.currentTimeMillis() - time >= 1500) {
-            time = System.currentTimeMillis()
-            snack.config(this)
-            snack.show()
-        } else if (System.currentTimeMillis() - time < 1500) {
-            finish()
-        }
-    }
-
-    private fun Snackbar.config(context: Context) {
-        val params = this.view.layoutParams as ViewGroup.MarginLayoutParams
-        params.setMargins(12, 12, 12, 12)
-        this.view.layoutParams = params
-        this.view.background = context.getDrawable(R.drawable.bg_snackbar)
-
-        ViewCompat.setElevation(this.view, 6f)
     }
 
     private val navListener = BottomNavigationView.OnNavigationItemSelectedListener {
@@ -68,29 +30,23 @@ class MainActivity : AppCompatActivity() {
             R.id.home -> {
                 val fragment = HomeFragment()
                 supportFragmentManager.beginTransaction()
-                    .replace(R.id.main_fragment_view, fragment, fragment.javaClass.simpleName).commit()
+                    .replace(R.id.main_frame, fragment, fragment.javaClass.simpleName).commit()
                 return@OnNavigationItemSelectedListener true
             }
             R.id.map -> {
                 val fragment = MapFragment()
                 supportFragmentManager.beginTransaction()
-                    .replace(R.id.main_fragment_view, fragment, fragment.javaClass.simpleName).commit()
+                    .replace(R.id.main_frame, fragment, fragment.javaClass.simpleName).commit()
                 return@OnNavigationItemSelectedListener true
             }
             R.id.favorite -> {
                 val fragment = FavoriteFragment()
                 supportFragmentManager.beginTransaction()
-                    .replace(R.id.main_fragment_view, fragment, fragment.javaClass.simpleName).commit()
+                    .replace(R.id.main_frame, fragment, fragment.javaClass.simpleName).commit()
                 return@OnNavigationItemSelectedListener true
             }
-            R.id.account -> {
-                val fragment = AccountFragment()
-                supportFragmentManager.beginTransaction()
-                    .replace(R.id.main_fragment_view, fragment, fragment.javaClass.simpleName).commit()
-                return@OnNavigationItemSelectedListener true
-            }
+
         }
         false
     }
 }
-
--- app/src/main/java/com/commit/ddang/Item/HomeItem.kt
+++
@@ -1,8 +0,0 @@
-package com.commit.ddang.Item
-
-data class Homefeed(val items: List<Item>)
-data class Item(
-    val title: String,
-    val telephone: String,
-    val address: String
-)
