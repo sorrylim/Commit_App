618486007acbb1d41366cd4cc81456507feb8dd3
sorrylim <52535550+sorrylim@users.noreply.github.com>
2019-08-07 17:15:17 +0900

    hi

--- app/src/main/java/com/commit/ddang/Adapter/RecyclerViewAdapter.kt
+++ app/src/main/java/com/commit/ddang/Adapter/RecyclerViewAdapter.kt
@@ -7,27 +7,30 @@ import androidx.recyclerview.widget.RecyclerView
 import com.commit.ddang.Item.Homefeed
 import com.commit.ddang.Item.Item
 import com.commit.ddang.R
-import kotlinx.android.synthetic.main.item_home.view.*
+
+import kotlinx.android.synthetic.main.item_view.view.*
 
 class RecyclerViewAdapter(val homefeed: Homefeed) : RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>() {
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.bindItems(homefeed.items[position])
-    }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val v = LayoutInflater.from(parent.context).inflate(R.layout.item_home, parent, false)
-        return ViewHolder(v)
-    }
 
     override fun getItemCount(): Int {
         return homefeed.items.count()
     }
 
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerViewAdapter.ViewHolder {
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.item_view, parent, false)
+        return ViewHolder(v)
+    }
+
+    //this method is binding the data on the list
+    override fun onBindViewHolder(holder: RecyclerViewAdapter.ViewHolder, position: Int) {
+        holder.bindItems(homefeed.items.get(position))
+    }
+
+    class ViewHolder(val view: View) : RecyclerView.ViewHolder(view){
         fun bindItems(data: Item) {
-            itemView.item_home_text_title.text = data.title.replace("<b>", "").replace("</b>", "")
-            itemView.item_home_text_telephone.text = "전화: ${data.telephone}"
-            itemView.item_home_text_address.text = "주소: ${data.address}"
+            itemView.textView_title.text = data.title
+            itemView.textView_address.text = data.address
         }
     }
 }
