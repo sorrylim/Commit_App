d7c7f7e2cb66da2c16b53c08fde00ce4df0e4409
sorrylim <52535550+sorrylim@users.noreply.github.com>
2019-08-14 18:13:30 +0900

    Update MainActivity.kt

--- app/src/main/java/com/commit/ddang/Activity/MainActivity.kt
+++ app/src/main/java/com/commit/ddang/Activity/MainActivity.kt
@@ -3,7 +3,6 @@ package com.commit.ddang.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import com.commit.ddang.Fragment.AccountFragment
 import com.commit.ddang.Fragment.FavoriteFragment
 import com.commit.ddang.Fragment.HomeFragment
 import com.commit.ddang.Fragment.MapFragment
@@ -35,36 +34,6 @@ class MainActivity : AppCompatActivity() {
                 .replace(R.id.main_frame, fragment, fragment.javaClass.simpleName).commit()
 
         }
-
-        val text:String = URLEncoder.encode(editText.text.toString(), "UTF-8")
-        val apiURL = "https://openapi.naver.com/v1/search/local.json"+ text
-
-        val url = URL(apiURL)
-        val con:HttpURLConnection = url.openConnection() as HttpURLConnection
-
-        con.setRequestMethod("GET")
-        con.setRequestProperty("X-Naver-Client-Id", clientId)
-        con.setRequestProperty("X-Naver-Client-Secret", clientSecret)
-
-        val responseCode:Int = con.getResponseCode()
-        var br:BufferedReader
-
-        if(responseCode == 200)
-        {
-            br = BufferedReader(InputStreamReader(con.getInputStream()))
-        }
-        else
-        {
-            br = BufferedReader(InputStreamReader(con.getErrorStream()))
-        }
-
-        var inputLine:String? = br.readLine()
-        var response:StringBuffer = StringBuffer()
-
-        while(inputLine != null) {
-            response.append(inputLine)
-        }
-        br.close()
     }
 
     private val navListener = BottomNavigationView.OnNavigationItemSelectedListener {
@@ -87,13 +56,6 @@ class MainActivity : AppCompatActivity() {
                     .replace(R.id.main_frame, fragment, fragment.javaClass.simpleName).commit()
                 return@OnNavigationItemSelectedListener true
             }
-            R.id.account -> {
-                val fragment = AccountFragment()
-                supportFragmentManager.beginTransaction()
-                    .replace(R.id.main_frame, fragment, fragment.javaClass.simpleName).commit()
-                return@OnNavigationItemSelectedListener true
-            }
-
         }
         false
     }
